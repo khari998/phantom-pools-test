1195bd6e4feee73ce2a9c1aff90720ae618229f4
Khari <khari.lane@khalikenterprises.com>
2020-09-04 12:23:06 -0400

    Memoized NameListItem to prevent re-renders successfully

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback, memo} from 'react';
 import './App.css';
+
+const ALL_NAMES = ["foo", "bar", "baz"];
+
+interface NameListItemProps {
+  readonly name: string;
+  readonly onNameClick: (clickedName: string) => void;
+}
+
+const NameListItem: React.FC<NameListItemProps> = memo(({ onNameClick, name}) =>  (
+  <li>
+      <button onClick={() => onNameClick(name)}>{name}</button>
+  </li>
+)); 
+
+const NameList: React.FC = memo(() => {
+  
+  const [lastClickedName, setLastClickedName] = React.useState<
+    string | undefined
+  >(undefined); 
+  
+  return (
+    <div>
+      <h1>
+        {lastClickedName === undefined
+          ? "No Name Clicked Yet"
+          : lastClickedName}
+      </h1>
+      <ul>
+        {ALL_NAMES.map((name: string, idx: number) => (
+          <NameListItem key={idx} name={name} onNameClick={useCallback(() => setLastClickedName(name), [name])}/>
+        ))}
+      </ul>
+    </div>
+  );
+});
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NameList/>
     </div>
   );
 }
